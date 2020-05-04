29c75ac1109dcacdd363624be41a756ae2d721ca
Ruoyu Zhu <ruoyuzhu@Ruoyus-MacBook-Pro.local>
2020-05-04 00:00:54 -0400

    function called

--- src/models/memory_based_recommender/test_function_ignore.py
+++ src/models/memory_based_recommender/test_function_ignore.py
@@ -1,9 +1,22 @@
-#test function on github
+import os
+#retval = os.getcwd()
+#print("Current working directory %s" % retval)
 
 import pandas as pd
 import numpy as np
 
-from load_copy import load_file
+# #** Put here the directory where you have the file with your function**
+# os.chdir("/Users/ruoyuzhu/Documents/3001-AdvancedPython/Final_Project/DS-GA-3001-Advanced-Python/src/common/preprocess")
+# retval = os.getcwd()
+# print("Current working directory %s" % retval)
+import sys
+sys.path.append("/Users/ruoyuzhu/Documents/3001-AdvancedPython/Final_Project/DS-GA-3001-Advanced-Python/src/")
+from common.preprocess.data_process import generate_userByItem, split, load_movies
 
-rating = load_file(f'/data/ratings.csv', ['userId', 'movieId', 'rating'])
-rating.shape
+
+
+path_movie = f"/Users/ruoyuzhu/Documents/3001-AdvancedPython/Final_Project/DS-GA-3001-Advanced-Python/data/ratings.csv"
+#os.chdir('/data/movies.csv'
+um = generate_userByItem(path_movie, p = True)
+
+print(um.values)

175edfe902312139fe35e160283afbd229c950ff
Annie LIAN <enyilian@a-10-27-25-204.dynapool.vpn.nyu.edu>
2020-05-05 02:05:55 -0400

    change file path

--- src/models/content/test_time.py
+++ src/models/content/test_time.py
@@ -4,10 +4,15 @@ import time
 import os
 
 if __name__ == '__main__':
-    file_path = os.path.abspath('ml-latest-small/movies.csv')
+    data_path = os.path.dirname(os.path.abspath('../..'))
+    file_path = os.path.join(data_path, 'data', 'movies.csv')
+
+    t0 = time.time()
     df = preprocess(file_path, 'genres')
     tfidf_matrix = create_tfidf_matrix(df, 'genres')
-
+    print('Time for Tfidf matrix preparation: {:.6f}'.format(time.time() - t0))
+    print('-' * 20)
+    
     sim_mat_fns = [create_sim_matrix_kernel, create_sim_matrix_cosine,
                    create_sim_matrix_distance, create_sim_matrix_dot,
                    create_sim_matrix_product, create_sim_matrix_operator,
@@ -28,8 +33,8 @@ if __name__ == '__main__':
         rec1 = content_recommender(df, sim_mat, movie1, n)
         rec2 = content_recommender(df, sim_mat, movie2, n)
         rec3 = content_recommender(df, sim_mat, movie3, n)
-        print('Top {} recommendations for {}:\n{}\n'.format(n, movie1, rec1))
-        print('Top {} recommendations for {}:\n{}\n'.format(n, movie2, rec2))
-        print('Top {} recommendations for {}:\n{}\n'.format(n, movie3, rec3))
+        # print('Top {} recommendations for {}:\n{}\n'.format(n, movie1, rec1))
+        # print('Top {} recommendations for {}:\n{}\n'.format(n, movie2, rec2))
+        # print('Top {} recommendations for {}:\n{}\n'.format(n, movie3, rec3))
         print('Time for {} is {:.6f}'.format(sim_name, time.time() - t))
         print('-' * 20)
